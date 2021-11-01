dc1fc7411b4249b180ff04b06c02276cedbfdc86
karlerik100 <karl.erik.kaselaan@ametikool.ee>
2021-11-01 13:41:33 +0200

    https://github.com/karlerik100/wtf.git

---
+++ Hello.py
@@ -0,0 +1,7 @@
+name1 = "Stan Lee"
+name2 = input("Nimi:")
+a = input("Number:")
+age = 30
+print(age)
+first_name = "Karl Erik" 
+print("Excelsior, " + name2 + "!")
---
+++ examp.py
@@ -0,0 +1,6 @@
+eek = input("Sisesta kroonid:")
+
+eur = float(eek) / 15.6466
+
+
+print(eur)
